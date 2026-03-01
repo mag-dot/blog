06745363a6c100df637c8f82bfe4675d995b87db
mag-dot <magshumc@gmail.com>
2026-03-01 16:38:24 +0000

    feat: add GTM GTM-N6FZ4H7D

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,7 +105,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" content="#ffffff" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8178097336205658" crossOrigin="anonymous"></script>
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N6FZ4H7D');`}} />
       <body className="bg-white pl-[calc(100vw-100%)] text-gray-900 antialiased">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6FZ4H7D" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
