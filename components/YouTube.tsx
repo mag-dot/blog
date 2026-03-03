5f4551f307e4a9781e4bc8447072e22a86dab2d4
mag-dot <magshumc@gmail.com>
2026-03-03 14:41:21 +0000

    Fix YouTube embed: use MDX component, move after first H2, fix dollar sign math rendering

---
+++ components/YouTube.tsx
@@ -0,0 +1,19 @@
+interface YouTubeProps {
+  id: string
+  title?: string
+}
+
+export default function YouTube({ id, title = 'YouTube video' }: YouTubeProps) {
+  return (
+    <div className="relative my-6 w-full overflow-hidden rounded-lg" style={{ paddingBottom: '56.25%' }}>
+      <iframe
+        className="absolute left-0 top-0 h-full w-full"
+        src={`https://www.youtube.com/embed/${id}`}
+        title={title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  )
+}
